c2e7aacdee84b8af3bc376b8866b1a4a5e61ea4b
M <mansory.php@gmail.com>
2025-07-07 15:44:35 +0330

    edit search

--- resources/views/search/error.blade.php
+++ resources/views/search/error.blade.php
@@ -4,7 +4,7 @@
         <h5 class="text-primary">{{ $category_select->parent->name }} > {{ $category_select->name }} > {{ $field->name }}</h5>
     </div>
     <div class="py-2 text-center">
-        <h5 class="">لطفا از این فیلد استفاده نکنید</h5>
+        <h5 class=""> لطفا از این فیلد {{ $field->name }} استفاده نکنید </h5>
         <h5 class="">یا این جستجو اشتباه است</h5>
     </div>
     <br><br>
