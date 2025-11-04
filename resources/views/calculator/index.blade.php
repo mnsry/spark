e298ee0e98a0e9968ba14789561a9062a66bffb9
M <mansory.php@gmail.com>
2025-11-04 20:01:47 +0330

    cal blade

---
+++ resources/views/calculator/index.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fa" data-bs-theme="auto">
+<head>
+    <script src="{{ asset('js/color-modes.js') }}"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ماشین حساب">
+    <meta name="author" content="مسعود منصوری">
+    <meta name="generator" content="مسعود منصوری">
+    <title>ماشین حساب</title>
+
+    <link href="{{ asset('css/css@3.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.rtl.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sign-in.css') }}" rel="stylesheet">
+
+    @laravelPWA
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+
+<main class="form-signin w-100 m-auto">
+    021
+</main>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 |--------------------------------------------------------------------------
 */
 Route::get('/', function () {return view('welcome');})->name('welcome');
-Route::get('/dev', function () {
-    return "test";
-})->name('dev');
+Route::get('/cal', function () {
+    return view('calculator.index');
+})->name('cal');
 Route::group(['prefix' => 'admin'], function () { Voyager::routes(); });
 Auth::routes();
 /*
