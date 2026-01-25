27f0869e72f8f0e0c51cd020823f2e2f8cb03ea2
mnsry <mansory.php@gmail.com>
2026-01-25 03:51:56 +0330

    test commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="bg-body-tertiary p-5 rounded mt-3">
         <h1>فایل ها موجود</h1>
-        <p> آخرین فایل » {{ $filesLast }} </p>
+        <p> آخرین فایل » {{ $filesLast->created_at->diffForHumans() }} </p>
         <a class="btn btn-lg btn-primary" href="{{ route('file.index') }}" role="button">تعداد » {{ $filesCount }} </a>
     </div>
 
